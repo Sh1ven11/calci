473f645bf4eae344d2798abf2b4a9c22e4b4bf43
Shiven005 <shivengupta.mitmpl2023@learner.manipal.edu>
2025-12-18 20:40:50 +0530

    Broken code

--- calci.py
+++ calci.py
@@ -1,8 +1,6 @@
 a= int (input("Enter first number"))
 b= int (input("Enter second number"))   
-print("1. Addition 2Subtraction")
+print("1. Addition ")
 choice = int (input("Enter your choice"))
 if choice == 1:
-    print("Addition is", a+b)
-elif choice == 2:
-    print("Subtraction not done yet")
+    print("Addition is", a*b)
