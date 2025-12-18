4fd76e011b3b39695c894a079b5c9347e0d91329
Shiven005 <shivengupta.mitmpl2023@learner.manipal.edu>
2025-12-18 20:44:50 +0530

    correct code with 3 functions

--- calci.py
+++ calci.py
@@ -3,4 +3,8 @@ b= int (input("Enter second number"))
 print("1. Addition ")
 choice = int (input("Enter your choice"))
 if choice == 1:
-    print("Addition is", a*b)
+    print("Addition is", a+b)
+if choice == 2:
+    print("Subtraction is", a-b)
+if choice == 3:
+    print("Multiplication is", a*b)
