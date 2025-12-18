9d0a5dfb71d98ef661bc536e606c4eebc226e6a1
Shiven005 <shivengupta.mitmpl2023@learner.manipal.edu>
2025-12-18 21:00:28 +0530

    Invalid print statement added

--- calci.py
+++ calci.py
@@ -1,10 +1,12 @@
 a= int (input("Enter first number"))
 b= int (input("Enter second number"))   
-print("1. Addition ")
+print("1. Addition 2Subtraction")
 choice = int (input("Enter your choice"))
 if choice == 1:
     print("Addition is", a+b)
-if choice == 2:
-    print("Subtraction is", a-b)
-if choice == 3:
-    print("Multiplication is", a*b)
+
+elif choice == 2:
+    print("Subtraction not done yet")
+else:
+    print("Invalid choice")
+
